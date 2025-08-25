50f328fc035ab57ceaad4a255d0da33789ddd142
superdonitsi <113589554+superdonitsi@users.noreply.github.com>
2025-08-25 17:54:14 +0300

    Update laskin.py

--- laskin.py
+++ laskin.py
@@ -8,3 +8,14 @@ def divide(x, y):
     if y == 0:
         return "Nollalla jakaminen ei ole sallittua!"
     return x / y
+    
+if valinta == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif valinta == '2':
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+elif valinta == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif valinta == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Virheellinen valinta")
